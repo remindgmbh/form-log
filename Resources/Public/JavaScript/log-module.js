34cb4738bb01deefc40cb350faa510d83f3936f9
David Mellen <d.mellen@remind.de>
2024-07-10 14:18:25 +0200

    fix: remove debug logging

--- Resources/Public/JavaScript/log-module.js
+++ Resources/Public/JavaScript/log-module.js
@@ -1,10 +1,8 @@
 const entries = document.getElementsByClassName('log-module__entry')
 for (let index = 0; index < entries.length; index++) {
     const entry = entries[index];
-    console.log(entry)
     const dataId = entry.id.replace('entry-', '')
     entry.addEventListener('click', () => {
-        console.log('click')
         const data = document.getElementById(`entry-data-${dataId}`)
         const cssClass = 'log-module__data--expanded'
         data.classList.contains(cssClass) ? data.classList.remove(cssClass) : data.classList.add(cssClass)
